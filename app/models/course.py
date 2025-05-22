8146c786a1eccf4df8b219fa30df4890f863b891
hooryaax <hooriaamir744@gmail.com>
2025-05-22 17:39:00 +0500

    Add login and authentication module

--- app/models/course.py
+++
@@ -1,6 +0,0 @@
-class Course:
-    def __init__(self, course_id, title, description, teacher_id):
-        self.course_id = course_id
-        self.title = title
-        self.description = description
-        self.teacher_id = teacher_id
