4f6f3a92a08e41405e29e855865ec01e29f289ef
hooryaax <hooriaamir744@gmail.com>
2025-05-22 17:44:44 +0500

    Fix nested repo issue

---
+++ CourseManagementSystem/Course-Enrolment_hooria/app/models/course.py
@@ -0,0 +1,6 @@
+class Course:
+    def __init__(self, course_id, title, description, teacher_id):
+        self.course_id = course_id
+        self.title = title
+        self.description = description
+        self.teacher_id = teacher_id
